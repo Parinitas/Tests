1526d0d0554613557273579a0d7e4f3c85e33c7b
Parinitas <parinitasadhukhan231092@gmail.com>
2019-01-29 15:57:37 +0530

    commnit

---
+++ src/main/java/Test.java
@@ -0,0 +1,22 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test t=new Test();	
+		t.add(5.0,6);
+		System.out.println("Hello World");
+	}
+
+	public  void add(int a,double b) {
+		
+		System.out.println("Added value in A");
+		
+	}
+	public  void add(double b,int a) {
+		
+		System.out.println("Added value in B");
+		
+	}
+	
+}
